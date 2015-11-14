71e5aa1a0c4b5d353a1975c1daeb30ce4450f7e1
mrjaimisra <misrajai01@gmail.com>
2015-11-14 15:13:49 -0700

    Generating and voting on a poll
    Why:
    
    * After a poll is created, the user should be redirected to a voting
      page with a unique hashed poll id as the uri, and the poll id should
      be saved to the poll data
    * The voterId of the user who created the poll should be saved to the
      poll data
    * The title of the voting page should be the question, and the responses
      should be a radio button group of the options
    * When the user clicks 'Submit', he or she should be redirected to the
      same page, with a message stating the response has been submitted, and
      they can use the re-rendered form to change that response
    
    This change addresses the need by:
    
    * Adding 'crypto' to package.json for hashing uris and unique ids
    * Adding 'ejs' to package.json for html form templating
    * Defining a polls = {} object to store poll data each time a poll is
      created or a vote is submitted
    * Writing post('/') route for creating new polls and generating hashed
      voterId's, poll id's and uri's
    * Writing get and post('/polls/:id') routes and writing the html
      templates that will be generated when those routes are hit

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const http = require('http');
 var express = require('express');
 var app = express();
 const _ = require('lodash');
+const crypto = require('crypto');
+const ejs = require('ejs');
+
+//ejs.delimiter = '%';
 
 /*
  * body-parser is a piece of express middleware that
@@ -18,17 +22,77 @@ var bodyParser = require('body-parser');
 
 // instruct the app to use the `bodyParser()` middleware for all routes
 app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const path = require('path');
 
 app.use(express.static('public'));
 
+const polls = {};
+const pollForm = `
+
+<html>
+  <body>
+    <h1>Question: <%= poll.pollQuestion %> </h1>
+      <h2>Choose one of the following: </h2>
+
+      <form action="/polls/<%= poll.id %>" method="post">
+      <% if ( poll.pollResponse.constructor === Array ) {
+        for (var i = 0; i < poll.pollResponse.length;  i++) { %>
+
+        <input type='radio'
+          value='<%= poll.pollResponse[i] %>'
+          name='responses'>
+          <%= poll.pollResponse[i] %>
+        <br>
+        <% }
+      } else { %>
+        <input type='radio' value='<%= poll.pollResponse %>'>
+          <%= poll.pollResponse %>
+      <% } %>
+        <br>
+        <button type='' class='submit'>Submit</button>
+      </form>
+  </body>
+</html>`;
+
+app.route('/polls/:id')
+  .get(function (req, res) {
+    const poll = polls[req.params.id];
+
+    res.send(ejs.render
+      (pollForm, {poll: poll, polls: polls})
+    );
+  })
+
+  .post(function (req, res) {
+    const poll = polls[req.params.id];
+
+    res.send('<h1>request submitted!</h1>' +
+             '<h2>use the form below to change your response</h2>' +
+              ejs.render(pollForm, {poll: poll, polls: polls} ));
+
+    console.log(polls);
+  });
+
 app.route('/')
-   .get(function (req, res) {
+  .get(function (req, res) {
     res.sendfile(__dirname + '/public/index.html');
-   })
-   .post(function (req, res) {
-    console.log(req.body.pollQuestion);
-    console.log(req.body.pollResponse);
-   });
+  })
+
+  .post(function (req, res) {
+    var poll = req.body;
+    var id = crypto.randomBytes(20).toString('hex');
+
+    poll.id = id;
+    poll.voterId = crypto.randomBytes(20).toString('hex');
+    poll.voterUrl = '/vote/' + poll.voterId;
+
+    polls[id] = poll;
+    res.redirect('/polls/' + id);
+  });
+
+//SERVER CONFIG STUFF
 
 const port = process.env.PORT || 3000;
 
